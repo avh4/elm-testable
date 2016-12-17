9b129b06e303bc760fe75bd3d7b612ee28c83903
Aaron VonderHaar <av@noredink.com>
2016-12-16 16:19:51 -0800

    Add negative test cases

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -6,20 +6,44 @@ import Html
 import TestContext exposing (TestContext)
 
 
+testEqual : Gen a -> String -> (a -> a -> Expect.Expectation) -> Test
+testEqual ( a, b ) name testCase =
+    Test.describe name
+        [ Test.test "when equal" <|
+            \() -> testCase a a
+        , Test.test "when not equal" <|
+            \() ->
+                testCase a b
+                    |> Expect.getFailure
+                    |> Maybe.map (always True)
+                    |> Maybe.withDefault False
+                    |> Expect.true ("Expected test case to fail when inputs are different: " ++ toString ( a, b ))
+        ]
+
+
+type alias Gen a =
+    ( a, a )
+
+
+string : Gen String
+string =
+    ( "Alpha", "Beta" )
+
+
 all : Test
 all =
     describe "Testable"
         [ describe "Model"
-            [ test "verifying an initial model" <|
-                \() ->
-                    { model = "Alpha"
+            [ testEqual string "verifying an initial model" <|
+                \actual expected ->
+                    { model = actual
                     , update = \_ _ -> "Beta"
                     , view = \_ -> Html.text ""
                     }
                         |> Html.beginnerProgram
                         |> TestContext.start
                         |> TestContext.model
-                        |> Expect.equal (Ok "Alpha")
+                        |> Expect.notEqual (Ok expected)
             ]
           -- , describe "Cmds" []
           -- , describe "Http" []
