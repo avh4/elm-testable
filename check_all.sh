4f2fa3124c3cb026d9bed23f0021fe4003626086
Rogerio Chaves <rogeriocfj+github@gmail.com>
2016-12-22 10:39:46 -0200

    Do not rely on global elm-test even for local development, and install everything on check_all

--- check_all.sh
+++ check_all.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+elm-package install --yes
+[ ! -d node_modules ] && npm install elm-test@0.17
+PATH=$PATH:$(pwd)/node_modules/.bin
+
 rm -Rf elm-stuff/build-artifacts
 rm -Rf tests/elm-stuff/build-artifacts
 rm -Rf examples/elm-stuff/build-artifacts
@@ -11,6 +15,7 @@ elm-make --yes
 ./run-tests.sh
 
 cd examples
+elm-package install --yes
 ./run-tests.sh
 elm-make --yes RandomGif.elm --output RandomGif.js
 elm-make --yes Spelling.elm --output Spelling.js
