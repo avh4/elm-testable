b002b7eafc5a262abd5414d88d6a531d4416e395
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-01-03 22:03:09 -0800

    Extract CmdTests from Tests

---
+++ tests/CmdTests.elm
@@ -0,0 +1,69 @@
+module CmdTests exposing (..)
+
+import Test exposing (..)
+import Expect
+import Html
+import TestContext exposing (TestContext)
+import TestPorts
+
+
+testEqual : Gen a -> String -> (a -> a -> Expect.Expectation) -> Test
+testEqual ( a, b ) name testCase =
+    Test.describe name
+        [ Test.test "when equal" <|
+            \() -> testCase a a
+        , Test.test "when not equal" <|
+            \() ->
+                testCase a b
+                    |> Expect.getFailure
+                    |> Maybe.map (always True)
+                    |> Maybe.withDefault False
+                    |> Expect.true ("Expected test case to fail when inputs are different: " ++ toString ( a, b ))
+        ]
+
+
+type alias Gen a =
+    ( a, a )
+
+
+string : Gen String
+string =
+    ( "Alpha", "Beta" )
+
+
+all : Test
+all =
+    describe "Cmds"
+        [ testEqual string "verifying an initial Cmd" <|
+            \actual expected ->
+                { init = ( (), TestPorts.string actual )
+                , update = \msg _ -> ( msg, Cmd.none )
+                , subscriptions = \_ -> Sub.none
+                , view = \_ -> Html.text ""
+                }
+                    |> Html.program
+                    |> TestContext.start
+                    |> TestContext.expectCmd (TestPorts.string expected)
+        , testEqual string "verifying a Cmd from update" <|
+            \actual expected ->
+                { init = ( (), Cmd.none )
+                , update = \msg _ -> ( msg, TestPorts.string actual )
+                , subscriptions = \_ -> Sub.none
+                , view = \_ -> Html.text ""
+                }
+                    |> Html.program
+                    |> TestContext.start
+                    |> TestContext.update ()
+                    |> TestContext.expectCmd (TestPorts.string expected)
+        , testEqual string "verifying an initial Cmd after an update" <|
+            \actual expected ->
+                { init = ( (), TestPorts.string actual )
+                , update = \msg _ -> ( msg, Cmd.none )
+                , subscriptions = \_ -> Sub.none
+                , view = \_ -> Html.text ""
+                }
+                    |> Html.program
+                    |> TestContext.start
+                    |> TestContext.update ()
+                    |> TestContext.expectCmd (TestPorts.string expected)
+        ]
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -4,8 +4,8 @@ import Test exposing (..)
 import Expect
 import Html
 import TestContext exposing (TestContext)
-import TestPorts
 import Task
+import CmdTests
 import HttpTests
 import ModelTests
 import SubTests
@@ -51,40 +51,7 @@ all =
     describe "Testable"
         [ Testable.TaskTests.all
         , ModelTests.all
-        , describe "Cmds"
-            [ testEqual string "verifying an initial Cmd" <|
-                \actual expected ->
-                    { init = ( (), TestPorts.string actual )
-                    , update = \msg _ -> ( msg, Cmd.none )
-                    , subscriptions = \_ -> Sub.none
-                    , view = \_ -> Html.text ""
-                    }
-                        |> Html.program
-                        |> TestContext.start
-                        |> TestContext.expectCmd (TestPorts.string expected)
-            , testEqual string "verifying a Cmd from update" <|
-                \actual expected ->
-                    { init = ( (), Cmd.none )
-                    , update = \msg _ -> ( msg, TestPorts.string actual )
-                    , subscriptions = \_ -> Sub.none
-                    , view = \_ -> Html.text ""
-                    }
-                        |> Html.program
-                        |> TestContext.start
-                        |> TestContext.update ()
-                        |> TestContext.expectCmd (TestPorts.string expected)
-            , testEqual string "verifying an initial Cmd after an update" <|
-                \actual expected ->
-                    { init = ( (), TestPorts.string actual )
-                    , update = \msg _ -> ( msg, Cmd.none )
-                    , subscriptions = \_ -> Sub.none
-                    , view = \_ -> Html.text ""
-                    }
-                        |> Html.program
-                        |> TestContext.start
-                        |> TestContext.update ()
-                        |> TestContext.expectCmd (TestPorts.string expected)
-            ]
+        , CmdTests.all
         , describe "Tasks"
             [ testEqual string "tasks in initial commands should immediately be processed" <|
                 \actual expected ->
