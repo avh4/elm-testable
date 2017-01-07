35745eb3447ef6140f09756dd0c82bf0cd1709fb
Aaron VonderHaar <gruen0aermel@gmail.com>
2017-01-07 08:39:46 -0800

    Add internal documentation to Mapper

--- src/Mapper.elm
+++ src/Mapper.elm
@@ -1,5 +1,27 @@
 module Mapper exposing (Mapper, apply, map)
 
+{-| INTERNAL DOCUMENTATION: DO NOT EXPOSE THIS MODULE!
+
+This is a module that uses native code to let you hide a type from Elm.
+When using the module, you must make sure that you call `apply` with a value
+of the correc type that the Mapper is expecting.
+
+For example, instead of
+
+    type alias MyType a b = { f : a -> b }
+
+using Mapper can hide the `a` type
+
+    type alias MyType b = { f : Mapper b }
+
+
+NOTE: There is no exposed function that allows you to create Mappers.
+You can only create Mappers directly in native code.
+To create one, simply create a function that takes a single parameter
+and returns `msg`.
+
+-}
+
 import Native.Mapper
 
 
@@ -7,6 +29,12 @@ type Mapper msg
     = Mapper_Native_
 
 
+{-|
+
+Returns Err if the native code can somehow determine that `value` is not the
+correct type that the Mapper is expecting.
+(But do not rely on this--you are responsible for making sure the types match.)
+-}
 apply : Mapper msg -> value -> Result String msg
 apply =
     Native.Mapper.apply
