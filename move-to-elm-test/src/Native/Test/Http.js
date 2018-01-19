afdb99432222e17bc41c5f4d2f9e7daa04eda8ba
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-01-19 00:06:35 -0800

    Reformat javascript code

--- move-to-elm-test/src/Native/Test/Http.js
+++ move-to-elm-test/src/Native/Test/Http.js
@@ -1,47 +1,47 @@
 var _user$project$Native_Test_Http = (function () { // eslint-disable-line no-unused-vars, camelcase
-    var Just = _elm_lang$core$Maybe$Just;
-    var Nothing = _elm_lang$core$Maybe$Nothing;
-    var identity = _elm_lang$core$Basics$identity;
+  var Just = _elm_lang$core$Maybe$Just;
+  var Nothing = _elm_lang$core$Maybe$Nothing;
+  var identity = _elm_lang$core$Basics$identity;
 
-    // done : Task x1 a1 -> Task x0 a0
-    var fromTask = F2(function(done, task) {
-        switch(task.ctor) {
-            case '_Task_andThen':
-                // task.callback : a2 -> Task x1 a1
-                return fromTask(function (a2) {
-                    return done(_elm_lang$core$Task$andThen(task.callback)(a2))
-                })(task.task)
+  // done : Task x1 a1 -> Task x0 a0
+  var fromTask = F2(function(done, task) {
+    switch(task.ctor) {
+      case '_Task_andThen':
+        // task.callback : a2 -> Task x1 a1
+        return fromTask(function (a2) {
+          return done(_elm_lang$core$Task$andThen(task.callback)(a2))
+        })(task.task)
 
-            case '_Task_onError':
-                // task.callback : x2 -> Task x1 a1
-                return fromTask(function (x2) {
-                    return done(_elm_lang$core$Task$onError(task.callback)(x2))
-                })(task.task)
+      case '_Task_onError':
+        // task.callback : x2 -> Task x1 a1
+        return fromTask(function (x2) {
+          return done(_elm_lang$core$Task$onError(task.callback)(x2))
+        })(task.task)
 
-            case '_Task_succeed':
-            case '_Task_fail':
-            case 'MockTask':
-                return Nothing
+      case '_Task_succeed':
+      case '_Task_fail':
+      case 'MockTask':
+        return Nothing
 
-            case '_Task_nativeBinding':
-                if (task.elmTestable.ctor == "Http_NativeHttp_toTask") {
-                    return Just({
-                        method: task.elmTestable.method,
-                        url: task.elmTestable.url,
-                        callback: function(response) {
-                            return done(task.elmTestable.callback(response));
-                        },
-                    });
-                } else {
-                    return Nothing
-                }
-
-            default:
-                throw new Error('Unknown task type: ' + task.ctor);
+      case '_Task_nativeBinding':
+        if (task.elmTestable.ctor == "Http_NativeHttp_toTask") {
+          return Just({
+            method: task.elmTestable.method,
+            url: task.elmTestable.url,
+            callback: function(response) {
+              return done(task.elmTestable.callback(response));
+            },
+          });
+        } else {
+          return Nothing
         }
-    });
 
-    return {
-        fromTask: fromTask(identity),
+      default:
+        throw new Error('Unknown task type: ' + task.ctor);
     }
+  });
+
+  return {
+    fromTask: fromTask(identity),
+  }
 })()
--- move-to-elm-test/src/Native/Test/Task.js
+++ move-to-elm-test/src/Native/Test/Task.js
@@ -1,52 +1,52 @@
 var _user$project$Native_Test_Task = (function () { // eslint-disable-line no-unused-vars, camelcase
-    var Just = _elm_lang$core$Maybe$Just;
-    var Nothing = _elm_lang$core$Maybe$Nothing;
-    var Ok = _elm_lang$core$Result$Ok;
-    var Err = _elm_lang$core$Result$Err;
+  var Just = _elm_lang$core$Maybe$Just;
+  var Nothing = _elm_lang$core$Maybe$Nothing;
+  var Ok = _elm_lang$core$Result$Ok;
+  var Err = _elm_lang$core$Result$Err;
 
-    function resolvedTask(task) {
-        switch (task.ctor) {
-            case '_Task_succeed':
-                return Just(Ok(task.value))
+  function resolvedTask(task) {
+    switch (task.ctor) {
+      case '_Task_succeed':
+        return Just(Ok(task.value))
 
-            case '_Task_fail':
-                return Just(Err(task.value))
+      case '_Task_fail':
+        return Just(Err(task.value))
 
-            case '_Task_andThen':
-                var inner = resolvedTask(task.task);
-                switch (inner.ctor) {
-                    case 'Nothing':
-                        return Nothing;
+      case '_Task_andThen':
+        var inner = resolvedTask(task.task);
+        switch (inner.ctor) {
+          case 'Nothing':
+            return Nothing;
 
-                    case 'Just':
-                        switch (inner._0.ctor) {
-                            case 'Ok': return resolvedTask(task.callback(inner._0._0));
-                            case 'Err': return inner;
-                        }
-                }
-
-            case '_Task_onError':
-                var inner = resolvedTask(task.task);
-                switch (inner.ctor) {
-                    case 'Nothing':
-                        return Nothing;
-
-                    case 'Just':
-                        switch (inner._0.ctor) {
-                            case 'Ok': return inner;
-                            case 'Err': return resolvedTask(task.callback(inner._0._0));
-                        }
-                }
-
-            case '_Task_nativeBinding':
-                return Nothing;
-
-            default:
-                throw new Error('Unknown task type: ' + task.ctor);
+          case 'Just':
+            switch (inner._0.ctor) {
+              case 'Ok': return resolvedTask(task.callback(inner._0._0));
+              case 'Err': return inner;
+            }
         }
-    }
 
-    return {
-        resolvedTask: resolvedTask,
+      case '_Task_onError':
+        var inner = resolvedTask(task.task);
+        switch (inner.ctor) {
+          case 'Nothing':
+            return Nothing;
+
+          case 'Just':
+            switch (inner._0.ctor) {
+              case 'Ok': return inner;
+              case 'Err': return resolvedTask(task.callback(inner._0._0));
+            }
+        }
+
+      case '_Task_nativeBinding':
+        return Nothing;
+
+      default:
+        throw new Error('Unknown task type: ' + task.ctor);
     }
+  }
+
+  return {
+    resolvedTask: resolvedTask,
+  }
 })()
