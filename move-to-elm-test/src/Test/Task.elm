7e62f5684dd63f3e20e85da1880947b58fe68e37
Aaron VonderHaar <gruen0aermel@gmail.com>
2018-01-24 09:32:14 -0800

    Add documentation for Test.Task, Test.Time

--- move-to-elm-test/src/Test/Task.elm
+++ move-to-elm-test/src/Test/Task.elm
@@ -4,6 +4,12 @@ import Native.Test.Task
 import Task
 
 
+{-| Gives information about whether a Task is a `Task.succeed` or `Task.fail`
+(possibly also transformed using `map`, `andThen`, `onError`).
+This will return `Just (Ok a)` if such a task resolves to succed,
+`Just (Err x)` if such a task resolves to an error,
+and `Nothing` if the given Task is any other kind of task.
+-}
 resolvedTask : Task.Task x a -> Maybe (Result x a)
 resolvedTask =
     Native.Test.Task.resolvedTask
--- move-to-elm-test/src/Test/Time.elm
+++ move-to-elm-test/src/Test/Time.elm
@@ -5,6 +5,11 @@ import Task exposing (Task)
 import Time exposing (Time)
 
 
+{-| Gives information about whether a Task is a `Time.now` task
+(possibly also transformed using `map`, `andThen`, `onError`).
+This will return `Just` with the callback function for such a task
+and will return `Nothing` if the given Task is any other kind of task.
+-}
 fromTask : Task x a -> Maybe (Time -> Task x a)
 fromTask =
     Native.Test.Time.fromTask
